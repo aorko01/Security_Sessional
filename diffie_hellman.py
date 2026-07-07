ec167186fd4ccb7dcfe10997d8e72e11679dfe87
aorko01 <aorko321@gmail.com>
2026-07-07 13:51:51 +0600

    added prime generation with n bits

--- diffie_hellman.py
+++ diffie_hellman.py
@@ -1,5 +1,13 @@
 import random
+import secrets
 
+
+def random_n_bits_odd(n):
+    x = secrets.randbits(n)
+    x|=(1<<(n-1))
+    x|=1
+    return x    
+    
 def miller_robin_prime_test(number:int,rounds:int):
     if(number==2 or number==3):
         return True
@@ -31,7 +39,14 @@ def miller_robin_prime_test(number:int,rounds:int):
     return True
         
 
-# def generate_key(Prime:int,generator:int):
+def generate_n_bit_prime(n:int):
+    while True:
+        p=random_n_bits_odd(n)
+        
+        if miller_robin_prime_test(p,100):
+            return p
+            break
 
-print("yes" if miller_robin_prime_test(7,100) else "NO")
+# print("yes" if miller_robin_prime_test(7,100) else "NO")
+print(generate_n_bit_prime(128))
     
